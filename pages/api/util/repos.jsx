182eebb5ff2ff4735b7b23bda2908edd13bb4048
Burger1w <104843190+Burger1w@users.noreply.github.com>
2022-10-08 22:28:16 +0300

    Update repos.jsx

--- pages/api/util/repos.jsx
+++ pages/api/util/repos.jsx
@@ -2,9 +2,9 @@ import fetch from 'isomorphic-unfetch';
 
 export default async function Repos(req, res) {
     res.json(
-        await (await fetch('https://api.github.com/users/swothh/repos', {
+        await (await fetch('https://api.github.com/users/burger1w/repos', {
             headers: {
-                'Authorization': 'token {YOUR_TOKEN}'
+                'Authorization': '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'
             } // get token: https://github.com/settings/tokens
         })).json()
     );
