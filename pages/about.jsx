8939447a1e30e05f93a14f98507fd2182e0263ad
Burger1w <104843190+Burger1w@users.noreply.github.com>
2022-10-08 22:09:56 +0300

    Update about.jsx

--- pages/about.jsx
+++ pages/about.jsx
@@ -6,7 +6,7 @@ export default function About() {
     return (
         <>
             <Head>
-                <title>About | Swôth</title>
+                <title>About | Kadybot</title>
             </Head>
             <div className="w-full my-5">
                 <Hero />
