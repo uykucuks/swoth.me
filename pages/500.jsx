d2b7c9b99cf518796509231f40b1e5df4d9631eb
Burger1w <104843190+Burger1w@users.noreply.github.com>
2022-10-08 22:12:36 +0300

    Update 500.jsx

--- pages/500.jsx
+++ pages/500.jsx
@@ -4,7 +4,7 @@ export default function NotFound() {
     return (
         <>
             <Head>
-                <title>500 | Swôth</title>
+                <title>500 | Kadybot</title>
             </Head>
             <h1 className="text-5xl text-white font-bold text-center mt-20">500</h1>
             <p className="text-gray-400 text-center mb-20 text-lg">Internal Server Error.</p>
